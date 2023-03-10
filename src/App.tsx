6ba4fae4b6212ef84fea781137029272f0ecc6ec
sososo1245 <93255307+sososo1245@users.noreply.github.com>
2023-03-11 08:51:54 +0900

    메인페이지 3과 로그인 페이지 제작

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Carousel from "components/common/Carousel";
+import Main from "components/page/Main";
+import Login from "components/page/Login";
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
+      <Login/>
       <CssBaseline />
       <Container maxWidth="lg">
         <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
-        {/* <Carousel /> */}
+        <Carousel />
+        <Main/>
       </Container>
     </React.Fragment>
   );
