3cc9a8aa6dac1e1e8369abe6604c7601eb8a100b
Yeonjun Jung <duswns12893@gmail.com>
2023-03-13 19:44:14 +0900

    Feat: 메인 페이지에 슬라이드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { GlobalStyle } from "./styles/global-styles";
 // import Main from "components/page/Main";
 import Login from "components/auth/Login"
 import SignUp from "components/auth/SignUp";
+import Main from "components/page/Main";
+
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <GlobalStyle />
-      {/* <Main /> */}
       <Login />
       <SignUp />
+      <Main />
     </React.Fragment>
   );
 }
