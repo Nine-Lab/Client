0edab6e674a0ee43d3861030e3e1d6f506dd8e9e
JiHwan <hg_07@naver.com>
2023-03-25 03:21:49 +0900

    fix: 침수 관련 데이터 추가

--- src/components/carousel/CarouselSection4.tsx
+++ src/components/carousel/CarouselSection4.tsx
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
-import ChartCotainer from "../chart/chartData3";
+import ChartCotainer from "../chart/ChartData3";
 
 const CarouselContainer = styled("div")`
     width: 100%;
